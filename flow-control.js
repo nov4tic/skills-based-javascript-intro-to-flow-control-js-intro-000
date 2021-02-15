319c21fd32d795d7e0e1c155b04a636257768a5b
Dev Frias <ddfrias14@gmail.com>
2021-02-15 08:01:43 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,8 +15,9 @@ if (age >= 13 && age <= 19) {
   return ' You are a teenager!'}
 else if (age =< 12) {
   return 'You are a kid'}
-else { return 'error'
-}}
+else { 
+  return 'error'
+}}}
 
 function ternaryTeenager(age) {
 
