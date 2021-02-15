a3e8b67b5d03bb6f3bd52b41fb1e385fdfb28f14
Dev Frias <ddfrias14@gmail.com>
2021-02-15 08:00:24 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -11,8 +11,12 @@ else {
 }}
 
 function ageChecker(age) {
-
-}
+if (age >= 13 && age <= 19) {
+  return ' You are a teenager!'}
+else if (age =< 12) {
+  return 'You are a kid'}
+else { return 'error'
+}}
 
 function ternaryTeenager(age) {
 
